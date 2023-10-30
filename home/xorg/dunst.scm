2bfd80d3034dfa4540542fb05b47b913740c8807
tobias <tobias@nights.rest>
2023-10-30 20:36:35 +0100

    Dunst: update and clean config
    Also removes Mako since I don't use it

--- home/xorg/dunst.scm
+++ home/xorg/dunst.scm
@@ -1,73 +1,97 @@
 (define-module (home xorg dunst)
+  #:use-module (guix gexp)
+
   #:use-module (gnu services)
+  #:use-module (gnu services shepherd)
   #:use-module (gnu home services)
-  #:use-module (gnu home-services wm)
-  #:use-module (home services dunst)
-  #:use-module (gnu packages xdisorg)
-  #:use-module (gnu packages chromium)
-  #:use-module (guix gexp))
+  #:use-module (gnu home services shepherd)
+
+  #:use-module ((gnu packages dunst) #:select (dunst)))
+
+(define dunstrc "
+[global]
+    monitor = 0
+    follow = none
+
+    width = (0, 500)
+    height = 300
+    origin = top-center
+    offset = 10x10
+
+    notification_limit = 3
+
+    separator_height = 1
+
+    padding = 2
+
+    horizontal_padding = 2
+
+    text_icon_padding = 0
+
+    frame_width = 1
+
+    frame_color = \"#ffffff\"
+
+    idle_threshold = 120
+
+    font = Sarasa UI TC 11
+
+    format = \"<b>%s</b>\n%b\"
+
+    show_age_threshold = 60
+
+    stack_duplicates = true
+
+    hide_duplicate_count = false
+
+    show_indicators = yes
+
+    min_icon_size = 32
+
+    max_icon_size = 32
+
+    sticky_history = yes
+
+    history_length = 20
+
+    mouse_left_click = do_action, close_current
+    mouse_middle_click = close_current
+    mouse_right_click = close_all
+
+[urgency_low]
+    background = \"#000000\"
+    foreground = \"#ffffff\"
+    timeout = 3
+
+[urgency_normal]
+    background = \"#000000\"
+    foreground = \"#ffffff\"
+    timeout = 3
+
+[urgency_critical]
+    background = \"#000000\"
+    foreground = \"#ffffff\"
+    timeout = 0
+
+[fullscreen_delay_everything]
+    fullscreen = delay
+[fullscreen_show_critical]
+    msg_urgency = critical
+    fullscreen = show
+")
 
 (define-public services
   (list
-   (service
-    home-dunst-service-type
-    (home-dunst-configuration
-     (config
-      `((global
-         ((follow . keyboard)
-
-          (geometry . "\"400x20-30+20\"")
-          (indicate_hidden . #t)
-          (timeout . 5)
-          (shrink . #f)
-          (transparency . 0)
-          (notification_height . 0)
-          (separator_height . 2)
-          (padding . 8)
-          (horizontal_padding . 8)
-          (frame_width . 3)
-          ;; (frame_color . "\"#8fbcbb\"")
-          ;; (separator_color . frame)
-          (sort . #t)
-          (idle_threshold . 12)
-          (font . "Cozette")
-          (line_height . 0)
-          (markup . full)
-          ;; (format . "\"<b>%s</b>\\n%b\"")
-          ;; (alignment . left)
-          (show_age_threshold . 60)
-          (word_wrap . #t)
-          (ellipsize . middle)
-          (ignore_newline . #f)
-          (stack_duplicates . #t)
-          (hide_duplicate_count . #f)
-          (show_indicators . #t)
-          (icon_position . off)
-          (max_icon_size . 32)
-          (sticky_history . #t)
-          (history_length . 20)
-          (mouse_left_click . do_action)
-          (mouse_middle_click . close_current)
-          (mouse_right_click . close_all)))
-
-        (shortcuts
-         ((close . ctrl+space)
-          (close_all . ctrl+shift+space)
-          (history . ctrl+grave)
-          (context . ctrl+shift+period)))
-
-        (urgency_low
-         ((background . "\"#262831\"")
-          (foreground . "\"#88c0d0\"")
-          (timeout . 5)))
-
-        (urgency_normal
-         ((background . "\"#262831\"")
-          (foreground . "\"#a3be8c\"")
-          (timeout . 5)))
-
-        (urgency_critical
-         ((background . "\"#262831\"")
-          (foreground . "\"#bf616a\"")
-          (frame_color . "\"#4c566a\"")
-          (timeout . 5)))))))))
+   (simple-service 'dunst-shepherd-service
+                   home-shepherd-service-type
+                   (list
+                    (shepherd-service
+                     (provision '(dunst))
+                     (stop  #~(make-kill-destructor))
+                     (start #~(make-forkexec-constructor
+                               (list #$(file-append dunst "/bin/dunst")
+                                     (string-append
+                                      "-conf " (getenv "XDG_CONFIG_HOME") "/dunst/dunstrc")))))))
+   (simple-service 'dunst-xdg-config
+                   home-xdg-configuration-files-service-type
+                   `(("dunst/dunstrc" ,(plain-file "dunstrc" dunstrc))))))
