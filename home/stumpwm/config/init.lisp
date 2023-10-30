926e010f99d4b3297f09a809bd3dd21acda3efe0
tobias <tobias@nights.rest>
2023-10-30 20:48:53 +0100

    stumpwm: update config

--- home/stumpwm/config/init.lisp
+++ home/stumpwm/config/init.lisp
@@ -1,32 +1,105 @@
 (in-package :stumpwm)
 ;; (asdf:load-system :sb-cltl2)
 
+;; (dolist (module '("cpu" "mem" "net" "battery-portable" "wifi" "notify" "pass"))
+;;   (add-to-load-path (format nil "~a/.guix-home/profile/share/common-lisp/sbcl/stumpwm-~a/"
+;;                             (uiop:getenv "HOME")
+;;                             module)))
+
 (require 'slynk)
 (defcommand start-slynk () ()
             (slynk:create-server :dont-close t :port 4006))
 (define-key *top-map* (kbd "s-s") "start-slynk")
+
+(defcommand run-nyxt () ()
+            (if (not (string= "" (run-shell-command "pidof nyxt" t)))
+                (run-shell-command "nyxt --remote --eval '(make-window)'")
+              (run-shell-command "nyxt")))
+
+(defcommand run () ()
+            (let ((selection (car (select-from-menu (current-screen)
+                                                    (list
+                                                     ;; Flatpak
+                                                     "discord"
+                                                     "discord-screenaudio"
+                                                     "steam"
+                                                     "spotify"
+                                                     "prism"
+                                                     "veloren"
+                                                     ;; Apps
+                                                     "nyxt"
+                                                     "qutebrowser"
+                                                     "emacs"
+                                                     "icecat"
+                                                     "urxvt"
+                                                     "obs"
+                                                     "zrythm"
+                                                     "blender"
+                                                     "kdenlive"
+                                                     "krita"
+                                                     "inkscape")
+                                                    "run:"))))
+              (cond ((string= selection "discord")
+                     (run-shell-command "flatpak run com.discordapp.Discord"))
+                    ((string= selection "discord-screenaudio")
+                     (run-shell-command "flatpak run de.shorsh.discord-screenaudio"))
+                    ((string= selection "steam")
+                     (run-shell-command "flatpak run com.valvesoftware.Steam"))
+                    ((string= selection "spotify")
+                     (run-shell-command "flatpak run com.spotify.Client"))
+                    ((string= selection "prism")
+                     (run-shell-command "flatpak run org.prismlauncher.PrismLauncher"))
+                    ((string= selection "veloren")
+                     (run-shell-command "flatpak run net.veloren.airshipper"))
+                    ((string= selection "nyxt")
+                     (run-nyxt))
+                    (t (run-shell-command selection)))))
+
+(defcommand power-menu () ()
+            (let ((selection (car (select-from-menu (current-screen) (list "loginctl poweroff" "loginctl reboot") "power menu:"))))
+              (run-shell-command selection)))
+
+;; (defcommand power-menu () ()
+;;   (let ((selection (car (select-from-menu (current-screen)
+;;                                              (list "shutdown"
+;;                                                    "reboot")
+;;                                              "power menu:"))))
+;;     (cond ((string= selection "shutdown")
+;;            (run-shell-command "shepherd shutdown"))
+;;           ((string= selection "reboot")
+;;            (run-shell-command "shepherd reboot")))))
+
+
+(grename "main")
+(gnewbg "browser")
+(gnewbg "steam")
+(gnewbg "game")
+
+(set-prefix-key (kbd "s-t"))
 
 (defun list-define-keys (map alist)
   "define key using alist."
   (loop for (key . command) in alist
         do (define-key map (kbd key) command)))
 
-(set-prefix-key (kbd "s-t"))
-
+;; TODO: F-keys for group switching and Win+F-key for original
 (list-define-keys *top-map*
                   '(("s-;" . "colon")
+                    ("s-Escape" . "power-menu")
 
-                    ("s-1" . "gselect Base")
-                    ("s-2" . "gselect Mail")
-                    ("s-3" . "gselect Console")
-                    ("s-4" . "gselect Work")
+                    ("s-1" . "gselect 1")
+                    ("s-2" . "gselect 2")
+                    ("s-3" . "gselect 3")
+                    ("s-4" . "gselect 4")
+                    ;; ("F1"  . "gselect main")
+                    ;; ("s-F1" . '(send-raw-key-or-seiomthbing))
 
                     ("s-h" . "move-focus left")  ("s-n" . "move-focus left")
                     ("s-j" . "move-focus down")  ("s-e" . "move-focus down")
                     ("s-k" . "move-focus up")    ("s-i" . "move-focus up")
                     ("s-l" . "move-focus right") ("s-o" . "move-focus right")
 
-                    ("C-s-h" . "hsplit")  ("C-s-n" . "hsplit")
+                    ("C-s-l" . "hsplit")  ("C-s-o" . "hsplit")
                     ("C-s-j" . "vsplit")  ("C-s-e" . "vsplit")
                     ;; ("C-s-k" . "move-focus up")    ("C-s-i" . "move-focus up")
                     ;; ("C-s-l" . "move-focus right") ("C-s-o" . "move-focus right")
@@ -41,25 +114,29 @@
                     ("C-s-K" . "exchange-direction up")    ("C-s-I" . "exchange-direction up")
                     ("C-s-L" . "exchange-direction right") ("C-s-O" . "exchange-direction right")
 
-                    ("s-n" . "next-in-frame") ("s-p" . "prev-in-frame")
+                    ("s-TAB" . "next-in-frame") ("s-ISO_Left_Tab" . "prev-in-frame")
 
                     ("s-f" . "toggle-float") ("s-F" . "fullscreen")
 
-                    ("s-q" . "kill-window") ("s-Q" . "remove-split")
+                    ("s-t" . "toggle-always-show") ("s-T" . "toggle-always-on-top")
+
+                    ("s-q" . "delete-window") ("s-Q" . "remove-split")
 
                     ("s-Return" . "exec emacsclient -cne '(switch-to-buffer nil)'")
+
+                    ;; Apps
+                    ("s-space" . "run")
 
                     ("XF86AudioRaiseVolume" . "exec playerctl volume 0.03+")
                     ("XF86AudioLowerVolume" . "exec playerctl volume 0.03-")
                     ("XF86AudioMute" . "exec pulsemixer --toggle-mute")
 
                     ("XF86AudioPlay" . "exec playerctl play-pause")
-                    ("XF86AudioPrev" . "exec playerctl next")
-                    ("XF86AudioNext" . "exec playerctl previous")
+                    ("XF86AudioNext" . "exec playerctl next")
+                    ("XF86AudioPrev" . "exec playerctl previous")
 
                     ("XF86MonBrightnessUp" . "backlight-up")
-                    ("XF86MonBrightnessDown" . "backlight-down")
-                    ))
+                    ("XF86MonBrightnessDown" . "backlight-down")))
 
 ;; (asdf:load-system :cpu)
 ;; (load-module "cpu")
@@ -68,7 +145,7 @@
 ;; (load-module "mem")
 
 ;; (asdf:load-system :disk)
-;; (load-module "disk")
+;; (load-module "net")
 
 ;; (asdf:load-system :battery-portable)
 ;; (load-module "battery-portable")
@@ -77,16 +154,25 @@
 ;; (load-module "notify")
 ;; (notify:notify-server-toggle)
 
-(setf *startup-message* nil)
+;; (load-module "pass")
+
+(setf *startup-message* nil
+      *suppress-frame-indicator* t)
 (setf *mouse-focus-policy* :click
       *float-window-modifiers* :SUPER)
 (setf *message-window-gravity* :top
       *input-window-gravity* :center
       *window-border-style* :thin)
-(setf *ignore-wm-honts* t)
+(setf *ignore-wm-hints* t)
 
 (setf *window-format* "%m%n%s%c")
 (setf *screen-mode-line-format* (list "[^B%n^b] %W^>%d"))
 (setf *time-modeline-string* "%a %b %e %k:%M")
 
 (enable-mode-line (current-screen) (current-head) t)
+
+(which-key-mode)
+
+;; FIXME: This sucks
+(run-shell-command "xsetroot -solid black")
+(run-shell-command "xrandr --output DP-0 --mode 3440x1440 --rate 144")
--- home/stumpwm/stumpwm.scm
+++ home/stumpwm/stumpwm.scm
@@ -2,22 +2,25 @@
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix build-system trivial)
+  #:use-module (guix build-system copy)
   #:use-module (guix licenses)
 
-  #:use-module ((gnu packages xorg) #:select (xhost xsetroot))
+  #:use-module ((gnu packages xorg) #:select (xhost xset xsetroot xrandr))
+  #:use-module ((gnu packages kde) #:select (kdeconnect))
   #:use-module ((gnu packages glib) #:select (dbus))
   #:use-module ((gnu packages music) #:select (playerctl))
   #:use-module ((gnu packages pulseaudio) #:select (pulsemixer))
   #:use-module ((gnu packages lisp) #:select (sbcl))
-  #:use-module ((gnu packages lisp-xyz) #:select (sbcl-slynk))
+  #:use-module (gnu packages lisp-xyz)
   #:use-module ((gnu packages wm) #:select (stumpwm
-                                            sbcl-stumpwm-ttf-fonts
+                                            ;; sbcl-stumpwm-ttf-fonts
                                             sbcl-stumpwm-cpu
                                             sbcl-stumpwm-mem
-                                            sbcl-stumpwm-disk
+                                            sbcl-stumpwm-net
+                                            sbcl-stumpwm-wifi
                                             sbcl-stumpwm-battery-portable
-                                            sbcl-stumpwm-notify))
+                                            sbcl-stumpwm-notify
+                                            sbcl-stumpwm-pass))
 
   #:use-module (gnu services)
   #:use-module (gnu home services))
@@ -25,12 +28,15 @@
 (define xsession
   #~(begin
       (system* #$(file-append xhost "/bin/xhost") "+SI:localuser:tobias")
-      (system* #$(file-append xsetroot "/bin/xsetroot") "-cursor_name" "left_ptr")
-      (system* #$(file-append xsetroot "/bin/xsetroot") "-solid" "#000000")
-      (system* #$(file-append dbus "/bin/dbus-launch") "--exit-with-session"
+      (if (string= (gethostname) "okarthel")
+          (system* #$(file-append xset "/bin/xset") "s" "off" "-dpms"))
+      (system* #$(file-append xsetroot "/bin/xsetroot") "-cursor_name" "left_ptr" "-solid" "black")
+      ;; (system* #$(file-append kdeconnect "/libexec/kdeconnectd"))
+      (system* #$(file-append dbus "/bin/dbus-run-session") ;; "--exit-with-session"
                #$(file-append stumpwm "/bin/stumpwm"))))
 
 
+;; https://issues.guix.gnu.org/50274
 (define stumpwm-contrib
   (let ((commit "7d1e57ca1e011e0e56059ebf6e833e0976904d2a")
         (revision "5"))
@@ -45,9 +51,14 @@
              (commit commit)))
        (file-name (git-file-name name version))
        (sha256 (base32 "1jap13725f4xnypb8yph2lqn91z6gl5fb2c4ygzj8zwxdvgfan0i"))))
-     (build-system trivial-build-system)
-     (arguments '(#:builder (begin (mkdir %output) #t)))
-     ;; (inputs `(("stumpwm" ,stumpwm "lib")))
+     (build-system copy-build-system)
+     (arguments '(#:install-plan
+                  '(("media" "media")
+                    ("minor-mode" "minor-mode")
+                    ("modeline" "modeline")
+                    ("util" "util"))))
+     ;; (arguments '(#:builder (begin (mkdir %output) #t)))
+     (inputs `(("stumpwm" ,stumpwm "lib")))
      (home-page "https://github.com/stumpwm/stumpwm-contrib")
      (synopsis "StumpWM extra modules")
      (description "This package provides extra modules for StumpWM.")
@@ -57,24 +68,37 @@
 (define-public packages
   (list playerctl
         pulsemixer
+        xrandr
+        xsetroot
 
         sbcl
         sbcl-slynk
         stumpwm
-        stumpwm-contrib
-        ;; sbcl-stumpwm-cpu
-        ;; sbcl-stumpwm-mem
-        ;; sbcl-stumpwm-disk
-        ;; sbcl-stumpwm-battery-portable
-        ;; sbcl-stumpwm-notify
-        ))
+        ;; stumpwm-contrib
+
+        ;; sbcl-clx
+        ;; cl-stumpwm
+
+        ;; notify
+        ;; sbcl-bordeaux-threads
+        ;; sbcl-dbus
+        ;; sbcl-xml-emitter
+
+        sbcl-stumpwm-cpu
+        sbcl-stumpwm-mem
+        sbcl-stumpwm-net
+        sbcl-stumpwm-battery-portable
+        sbcl-stumpwm-wifi
+        sbcl-stumpwm-notify
+        sbcl-stumpwm-pass))
 
 (define-public services
   (list
    (simple-service 'nyxt-config
                    home-files-service-type
                    `((".xsession"            ,(program-file "xsession" xsession))
+                     (".xinitrc"             ,(program-file "xinitrc" xsession))
                      (".stumpwm.d/init.lisp" ,(local-file "config/init.lisp"))
-                     ;; (".stumpwm.d/contrib"   ,(git-fetch "https://github.com/stumpwm/stumpwm-contrib" "1jap13725f4xnypb8yph2lqn91z6gl5fb2c4ygzj8zwxdvgfan0i"))
-                     ))))
+                     ;; (".stumpwm.d/contrib"   ,(file-append stumpwm-contrib))
+                     )))) ; again, https://issues.guix.gnu.org/50274
 
