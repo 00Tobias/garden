0081976589f48f3eb64a4968efb2cd77adc9e8f6
tobias <tobias@nights.rest>
2024-03-14 01:22:35 +0100

    channels: add gaming and science channels (both unfree)

--- channels.scm
+++ channels.scm
@@ -10,11 +10,27 @@
           (openpgp-fingerprint
            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
        (channel
+        (name 'guix-science-nonfree)
+        (url "https://github.com/guix-science/guix-science-nonfree.git")
+        (introduction
+         (make-channel-introduction
+          "58661b110325fd5d9b40e6f0177cc486a615817e"
+          (openpgp-fingerprint
+           "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+       (channel
+        (name 'guix-gaming-games)
+        (url "https://gitlab.com/guix-gaming-channels/games.git")
+        (introduction
+         (make-channel-introduction
+          "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+          (openpgp-fingerprint
+           "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+       (channel
         (name 'rde)
         (url "https://git.sr.ht/~abcdw/rde")
-	    (introduction
-	     (make-channel-introduction
-	      "257cebd587b66e4d865b3537a9a88cccd7107c95"
-	      (openpgp-fingerprint
-	       "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+	      (introduction
+	       (make-channel-introduction
+	        "257cebd587b66e4d865b3537a9a88cccd7107c95"
+	        (openpgp-fingerprint
+	         "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
        %default-channels)
