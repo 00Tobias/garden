4ebdaf5043b131bf6b638810aabe0eca8e438602
tobias <tobias@nights.rest>
2024-05-09 01:57:11 +0200

    mpv: add scripts, update config, and clean definitions

--- home/mpv/mpv.scm
+++ home/mpv/mpv.scm
@@ -1,89 +1,59 @@
 (define-module (home mpv mpv)
   #:use-module (guix gexp)
-  #:use-module (guix transformations)
   #:use-module (guix packages)
   #:use-module (guix git-download)
 
-  #:use-module ((gnu packages video) #:select (ffmpeg mpv))
-  #:use-module ((gnu packages xorg) #:select (libxfixes))
-  #:use-module ((gnu packages vulkan) #:select (vulkan-loader))
-  #:use-module ((gnu packages video) #:select (libvdpau libvdpau-va-gl))
+  #:use-module ((gnu packages video) #:select (ffmpeg mpv yt-dlp))
+  #:use-module ((gnu packages curl) #:select (curl))
+
   #:use-module (gnu services)
   #:use-module (gnu home services)
 
-  #:use-module (nongnu packages nvidia))
+  #:use-module ((nongnu packages nvidia) #:select (replace-mesa)))
 
-(define transform
-  (options->transformation
-   '((with-graft . "mesa=nvda"))))
-
-(define ffmpeg-fruc
+(define ffmpeg-git
   (package
-   (inherit ffmpeg)
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/philipl/FFmpeg")
-           (commit "d84becd3da465cdf3d8a6400bdba71646800898b")))
-     ;; (file-name (git-file-name name version))
-     (sha256 (base32 "1a9fsk2hqy1a5g7dbl3yg60yh6r441mizzpf68jmcccynk1a04xg"))))))
+    (inherit ffmpeg)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.ffmpeg.org/ffmpeg.git")
+             (commit "575efc0406864542a65aa3f31ebe8eb0bbef5087")))
+       (sha256 (base32 "05cfn3z0267v7c0d2yvcmjbcf42hha9s17fr8fr1in2h4q0f4lhk"))))))
 
-(define-public eglexternalplatform
-  (let ((base (@ (gnu packages xorg) eglexternalplatform)))
-    (package
-     (inherit base)
-     (native-search-paths
-      (list (search-path-specification
-             (variable "__EGL_EXTERNAL_PLATFORM_CONFIG_DIRS")
-             (files '("share/egl/egl_external_platform.d"))))))))
-
-(define-public libglvnd
-  (let ((base (@ (gnu packages gl) libglvnd)))
-    (package
-     (inherit base)
-     (native-search-paths
-      (list (search-path-specification
-             (variable "__EGL_VENDOR_LIBRARY_DIRS")
-             (files '("share/glvnd/egl_vendor.d"))))))))
-
-;; (define-public mpv-nvidia
-;;   (let ((base (@ (gnu packages video) mpv)))
-;;     (package/inherit base
-;;      (name "mpv-nvidia")
-;;      (native-search-paths
-;;       (list (search-path-specification
-;;              (variable "VK_ICD_PATH")
-;;              (files '("share/vulkan/icd.d")))
-;;             (search-path-specification
-;;              (variable "VK_ILAYER_PATH")
-;;              (files '("share/vulkan/implicit_layer.d"))))))))
-
-(define mpv-nvidia
+(define mpv-git
   (package
-   (inherit mpv)
-   (inputs
-    (modify-inputs (package-inputs mpv)
-                   (prepend vulkan-loader eglexternalplatform libglvnd libvdpau libvdpau-va-gl libxfixes)
-                   (replace "ffmpeg" ffmpeg-fruc)
-                   (replace "mesa" nvda)))
-   (native-search-paths
-    (list (search-path-specification
-           (variable "VK_ICD_PATH")
-           (files '("share/vulkan/icd.d")))
-          (search-path-specification
-           (variable "VK_ILAYER_PATH")
-           (files '("share/vulkan/implicit_layer.d")))))))
+    (inherit mpv)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/mpv-player/mpv")
+             (commit "17be6e1990a92e9b43f5f49cca9c4dd3da24a1e8")))
+       (sha256 (base32 "1pr698qw5l6awfrc61j5hxcw2l56sxfq6ymd36ccpxhpnlzr3sgd"))))
+    (propagated-inputs
+     (modify-inputs (package-propagated-inputs mpv)
+       (replace "ffmpeg" ffmpeg-git)))))
 
 (define-public packages
-  (list (if (string= (gethostname) "okarthel")
-            (replace-mesa mpv)
-            mpv)))
+  (let ((lst (list
+              mpv-git
+              yt-dlp
+              curl                      ; sponsorblock_minimal.lua
+              )))
+    (if (string= (gethostname) "okarthel")
+        (map replace-mesa lst)
+        lst)))
 
 (define-public services
   (list
+   (simple-service 'mpv-env-vars-service
+                   home-environment-variables-service-type
+                   `(("WHISPER_CMD" . "1")))
    (simple-service 'mpv-config
                    home-xdg-configuration-files-service-type
-                   `(("mpv/mpv.conf"   ,(local-file "files/mpv.conf"))
-                     ;; ("mpv/input.conf"   ,(local-file "files/input.conf"))
-                     ("mpv/FSRCNNX_x2_8-0-4-1.glsl"   ,(local-file "files/FSRCNNX_x2_8-0-4-1.glsl"))))))
+                   `(("mpv/mpv.conf"                ,(local-file "files/mpv.conf"))
+                     ("mpv/input.conf"              ,(local-file "files/input.conf"))
+                     ("mpv/FSRCNNX_x2_8-0-4-1.glsl" ,(local-file "files/FSRCNNX_x2_8-0-4-1.glsl"))
+                     ("mpv/scripts"                 ,(local-file "files/scripts" #:recursive? #t))))))
