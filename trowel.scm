1012ec588455a60a572a2651dd6703149df5c33c
Tobias <tobias@nights.rest>
2025-11-18 23:22:26 +0100

    trowel: add wrap-package, and redefine aggressively-optimize with it

--- trowel.scm
+++ trowel.scm
@@ -2,20 +2,74 @@
   #:use-module (ice-9 threads)
 
   #:use-module (guix gexp)
-  #:use-module (guix utils)
   #:use-module (guix packages)
+  #:use-module (guix build-system trivial)
   #:use-module (guix build-system gnu)
 
-  #:use-module (nonguix utils)
-
   #:use-module ((gnu packages mold) #:select (mold-as-ld-wrapper))
-  #:use-module ((gnu packages gcc) #:select (gcc-15))
+  #:use-module ((gnu packages commencement) #:select (gcc-toolchain-15))
   #:use-module ((gnu packages bash) #:select (bash))
 
-  #:export (aggressively-optimize))
+  #:export (wrap-package aggressively-optimize))
 
-;; TODO: Wrapper package that just has the scripts? To avoid compiling GCC
-(define (aggressively-optimize p)
+(define* (wrap-package base #:key (flags '()) (env '()) (binaries '()) (unite '()))
+  "Wrap the given BASE package with the operations passed.
+FLAGS is a list of strings.
+ENV is an associative list of environment variables and values.
+BINARIES is a list of binary names to wrap with either FLAGS or ENV.
+UNITE is a list of packages to union-build together."
+  (package
+    (name (string-append (package-name base) "-wrapped"))
+    (version (package-version base))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments
+     (list
+      #:modules '((guix build union)
+                  (guix build utils))
+      #:builder
+      #~(begin
+          (use-modules (guix build union)
+                       (guix build utils))
+          (let ((flags? (not (null? '#$flags)))
+                (env? (not (null? '#$env))))
+            (union-build #$output (list #$@unite #$base) #:create-all-directories? #t)
+            (when (or flags? env?)
+              (for-each
+               (lambda (name)
+                 (let ((name-real (string-append name "-real")))
+                   (rename-file name name-real)
+                   (call-with-output-file name
+                     (lambda (port)
+                       (display
+                        (string-append
+                         (if env? (string-append
+                                   (string-join
+                                    (map (lambda (var)
+                                           (string-append "export " (car var) "=" (cdr var)))
+                                         '#$env)
+                                    "\n")
+                                   "\n")
+                             "")
+                         "#!" #$bash "/bin/bash\n"
+                         "exec " name-real " \"$@\" "
+                         (if flags? (string-join '#$flags " ") ""))
+                        port)))
+                   (chmod name #o555)))
+               (map (lambda (bin)
+                      (string-append #$output "/bin/" bin))
+                    '#$binaries)))))))
+    (native-search-paths (package-native-search-paths base))
+    (search-paths (package-search-paths base))
+    (inputs (map (lambda (package)
+                   (list (package-name package) package))
+                 (append (list bash) (list base) (if (not (null? unite)) unite '()))))
+    (synopsis (package-synopsis base))
+    (description (package-description base))
+    (home-page (package-home-page base))
+    (license (package-license base))))
+
+(define* (aggressively-optimize p #:key (toolchain gcc-toolchain-15))
   (package-with-c-toolchain
    (package
      (inherit p)
@@ -23,52 +77,24 @@
       (modify-inputs (package-native-inputs p)
         (prepend mold-as-ld-wrapper))))
    `(("gcc-toolchain-optimized"
-      ,((@@ (gnu packages commencement) make-gcc-toolchain)
-        (package
-          (inherit gcc-15)
-          (inputs
-           (modify-inputs (package-inputs gcc-15)
-             (prepend bash)))
-          (arguments
-           (substitute-keyword-arguments (package-arguments gcc-15)
-             ((#:phases phases)
-              #~(modify-phases #$phases
-                  (add-after 'install 'wrap-executables
-                    (lambda* (#:key inputs outputs #:allow-other-keys)
-                      (let* ((out (assoc-ref outputs "out"))
-                             (bash (string-append (assoc-ref inputs "bash") "/bin/bash")))
-                        (for-each
-                         (lambda (name)
-                           (let ((name-real (string-append name "-real")))
-                             (rename-file name name-real)
-                             (call-with-output-file name
-                               (lambda (port)
-                                 (format port
-                                         "#!~a~%exec \"~a\" \"$@\" ~a~%"
-                                         bash
-                                         name-real
-                                         #$(string-append
-                                            "-O3 "
-                                            "-march=native "
-                                            "-pipe "
+      ,(wrap-package toolchain
+                     #:binaries (list "gcc" "g++" "cpp" "c++")
+                     #:flags (list "-O3"
+                                   "-march=native"
+                                   "-pipe"
 
-                                            "-fuse-linker-plugin "
-                                            "-flto=" (number->string (total-processor-count))
-                                            " -fno-plt "
+                                   "-fuse-linker-plugin"
+                                   (string-append "-flto=" (number->string (total-processor-count)))
+                                   "-fno-plt"
 
-                                            "-fgraphite-identity "
-                                            "-floop-nest-optimize "
+                                   "-fgraphite-identity"
+                                   "-floop-nest-optimize"
 
-                                            "-fipa-pta "
-                                            "-fno-semantic-interposition "
-                                            "-fdevirtualize-at-ltrans "
+                                   "-fipa-pta"
+                                   "-fno-semantic-interposition"
+                                   "-fdevirtualize-at-ltrans"
 
-                                            "-fno-signed-zeros "
-                                            "-fno-trapping-math "
-                                            "-fassociative-math "
-                                            "-freciprocal-math"))))
-                             (chmod name #o555)))
-                         `(,(string-append out "/bin/gcc")
-                           ,(string-append out "/bin/g++")
-                           ,(string-append out "/bin/cpp")
-                           ,(string-append out "/bin/c++"))))))))))))))))
+                                   "-fno-signed-zeros"
+                                   "-fno-trapping-math"
+                                   "-fassociative-math"
+                                   "-freciprocal-math"))))))
