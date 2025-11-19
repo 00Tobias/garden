f116fe86ddab6fb69bae7300516d37b11e5229f8
Tobias <tobias@nights.rest>
2025-11-19 18:29:20 +0100

    emacs: big cleanup
    - Define packages in incognita instead
    - Switch off emacs-master
    - Remove emacs-magit changes
    - Add emacs-minuet and emacs-pinentry

--- home/emacs/emacs.scm
+++ home/emacs/emacs.scm
@@ -4,12 +4,10 @@
   #:use-module (guix transformations)
   #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix build-system emacs)
-  #:use-module ((gnu packages cmake) #:select (cmake-minimal))
   #:use-module ((guix licenses) #:prefix license:)
 
   #:use-module (gnu packages)
-  #:use-module ((gnu packages emacs) #:select (emacs-next emacs-next-pgtk))
+  #:use-module ((gnu packages emacs) #:select (emacs-next-pgtk emacs-next))
   #:use-module ((gnu packages gtk) #:select (gtk+))
   #:use-module ((gnu packages rust-apps) #:select (ripgrep))
   #:use-module ((gnu packages enchant) #:select (enchant))
@@ -23,17 +21,14 @@
                                              texlive-ulem
                                              texlive-capt-of))
   #:use-module ((gnu packages ocaml) #:select (emacs-tuareg))
-  #:use-module ((gnu packages emacs-build) #:select (makel))
   #:use-module (gnu packages emacs-xyz)
 
   #:use-module ((nongnu packages nvidia) #:select (replace-mesa))
 
-  #:use-module ((emacs-master) #:select (emacs-master-pgtk emacs-master))
+  #:use-module (incognita packages emacs-xyz)
 
-  #:use-module (gnu services)
   #:use-module (gnu home services)
   #:use-module (rde home services emacs)
-  #:use-module (gnu home-services-utils)
 
   #:use-module ((trowel) #:select (aggressively-optimize))
   #:use-module ((home theme) #:prefix theme:))
@@ -44,8 +39,8 @@
          ;; NOTE: Emacs on the default Guix version of gtk+ segfaults when closing a frame on gdk_window_get_screen. Updating gtk seems to fix the issue.
          (replace-mesa
           (package
-            (inherit emacs-master-pgtk)
-            (inputs (modify-inputs (package-inputs emacs-master-pgtk)
+            (inherit emacs-next-pgtk)
+            (inputs (modify-inputs (package-inputs emacs-next-pgtk)
                       (replace "gtk+"
                         (package
                           (inherit gtk+)
@@ -67,254 +62,14 @@
                              ((#:phases phases)
                               #~(modify-phases #$phases
                                   (delete 'disable-failing-tests))))))))))))
-        (else (emacs-master))))
-
-(define emacs-ultra-scroll
-  (let ((commit "93cd969c2ed1e75a950e6dec112a0ab1e4a6903b"))
-    (package
-      (name "emacs-ultra-scroll")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/jdtsmith/ultra-scroll")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/jdtsmith/ultra-scroll")
-                      (commit commit)))
-                (sha256 (base32 "1r9940l361jjrir59gvs5ivlpsgjhs13cwbhqy8j3zsmxf45z3qc"))))
-      (build-system emacs-build-system)
-      (synopsis "Scroll Emacs like lightning")
-      (description "ultra-scroll is a smooth-scrolling package for emacs, with
-native support for standard builds as well as emacs-mac.")
-      (license license:gpl3+))))
-
-(define emacs-flymake-posframe
-  (let ((commit "fadf75a51a9ab479e3ad00e23b7f38cbadaec460"))
-    (package
-      (name "emacs-flymake-posframe")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/Ladicle/flymake-posframe")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/Ladicle/flymake-posframe")
-                      (commit commit)))
-                (sha256 (base32 "0css70ncc9ygv932mf89schh86ybcw5qx4fg897lrbrma99fc9w2"))))
-      (inputs (list emacs-posframe))
-      (build-system emacs-build-system)
-      (arguments
-       (list
-        #:phases
-        #~(modify-phases %standard-phases
-            (add-after 'unpack 'patch-for-flymake-1.4
-              (lambda _
-                (substitute* "flymake-posframe.el"
-                  (("flymake--diag-text") "flymake-diagnostic-text")))))))
-      (synopsis "Showing flymake diagnostics at point using posframe")
-      (description "Showing flymake diagnostics at point using posframe.")
-      (license license:gpl3+))))
-
-(define emacs-nerd-icons-completion
-  (let ((commit "8e5b995eb2439850ab21ba6062d9e6942c82ab9c"))
-    (package
-      (name "emacs-nerd-icons-completion")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/rainstormstudio/nerd-icons-completion")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/rainstormstudio/nerd-icons-completion")
-                      (commit commit)))
-                (sha256 (base32 "0nbyrzz5sscycbr1h65ggzrm1m9agfwig2mjg7jljzw8dk1bmmd2"))))
-      (inputs (list emacs-nerd-icons))
-      (build-system emacs-build-system)
-      (synopsis "Use nerd-icons for completion")
-      (description "Use nerd-icons for completion.")
-      (license license:gpl3+))))
-
-(define emacs-nerd-icons-dired
-  (let ((commit "c0b0cda2b92f831d0f764a7e8c0c6728d6a27774"))
-    (package
-      (name "emacs-nerd-icons-dired")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/rainstormstudio/nerd-icons-dired")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/rainstormstudio/nerd-icons-dired")
-                      (commit commit)))
-                (sha256 (base32 "1iwqzh32j6fsx0nl4y337iqkx6prbdv6j83490riraklzywv126a"))))
-      (inputs (list emacs-nerd-icons))
-      (build-system emacs-build-system)
-      (synopsis "Use nerd-icons for Dired")
-      (description "Use nerd-icons for Dired.")
-      (license license:gpl3+))))
-
-(define emacs-nerd-icons-corfu
-  (let ((commit "41110180ceab9d0edaa856d19633b2b3fdf82e75"))
-    (package
-      (name "emacs-nerd-icons-corfu")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/LuigiPiucco/nerd-icons-corfu")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/LuigiPiucco/nerd-icons-corfu")
-                      (commit commit)))
-                (sha256 (base32 "0mwng5khhq6iqmr0ip8fv227cnkv0mv5664qz57r7sbjplqyabgf"))))
-      (inputs (list emacs-nerd-icons))
-      (build-system emacs-build-system)
-      (synopsis "Icons for corfu via nerd-icons")
-      (description "Nerd-icons-corfu.el is a library for adding icons to completions in Corfu.")
-      (license license:gpl3+))))
-
-(define emacs-ocaml-eglot
-  (let ((commit "387e53ce2ce4970e7f754013f57a4e41933de016"))
-    (package
-      (name "emacs-ocaml-eglot")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/tarides/ocaml-eglot")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/tarides/ocaml-eglot")
-                      (commit commit)))
-                (sha256 (base32 "1c8viz2gpmm6cilxjbk4r4j5bzpl9954amrz2gfl1g84fgycxhqa"))))
-      (build-system emacs-build-system)
-      (arguments
-       (list
-        ;; Linting the package returns errors during 'check'
-        #:test-command #~(list "make" "test-ert")
-        #:phases
-        #~(modify-phases %standard-phases ; From emacs-xyz.scm
-            (add-after 'unpack 'inject-makel
-              (lambda* (#:key inputs #:allow-other-keys)
-                (symlink (search-input-file inputs "/include/makel.mk")
-                         "makel.mk"))))))
-      (inputs (list makel))
-      (synopsis "An overlay on Eglot for editing OCaml code using LSP")
-      (description "ocaml-eglot is a lightweight Emacs minor mode designed to
-enhance the experience of writing OCaml code by leveraging the Eglot Language
-Server Protocol (LSP) client.")
-      (license license:x11))))          ; MIT
-
-(define emacs-cargo-transient
-  (let ((commit "b75511f911189b6b6c47976dd970eeb80ccfb3ee"))
-    (package
-      (name "emacs-cargo-transient")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/peterstuart/cargo-transient")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/peterstuart/cargo-transient")
-                      (commit commit)))
-                (sha256 (base32 "1s1kjc7xg55lnwn5ngdp89hnh2qp42x10cyqlji268xiglx0xzs2"))))
-      (inputs (list emacs-transient))
-      (build-system emacs-build-system)
-      (synopsis "A transient UI for Cargo, Rust's package manager")
-      (description "cargo-transient provides a transient interface for cargo.")
-      (license license:gpl3+))))
-
-(define emacs-uiua-mode
-  (let ((commit "8f9c8abb710b3f3ae8320e89e3c91612c7dd6f45"))
-    (package
-      (name "emacs-uiua-mode")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/crmsnbleyd/uiua-mode")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/crmsnbleyd/uiua-mode")
-                      (commit commit)))
-                (sha256 (base32 "09zlam421pkgv5y441k4f7j3wzrzwvdy1795slwvri437iipfscp"))))
-      (inputs (list emacs-reformatter))
-      (build-system emacs-build-system)
-      (synopsis "Emacs mode for uiua, a stack-based array language")
-      (description "Emacs mode for uiua, a stack-based array language.")
-      (license license:gpl3+))))
-
-(define emacs-uiua-ts-mode
-  (let ((commit "afa8e70ff3293182fc0412ba42e5733b80164c33"))
-    (package
-      (name "emacs-uiua-ts-mode")
-      (version (git-version "0" "1" commit))
-      (home-page "https://github.com/crmsnbleyd/uiua-ts-mode")
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/crmsnbleyd/uiua-ts-mode")
-                      (commit commit)))
-                (sha256 (base32 "0xq3y93mn42m98zfgqb8w73z10fcd3z2p2lmsxvybb49b69rik1k"))))
-      (inputs (list emacs-uiua-mode emacs-reformatter))
-      (build-system emacs-build-system)
-      (synopsis "Emacs major mode for Uiua, a stack-based array language, powered by tree-sitter")
-      (description "Emacs major mode for Uiua, a stack-based array language, powered by tree-sitter.")
-      (license license:gpl3+))))
-
-(define emacs-hotfuzz
-  (let ((commit "48fcdae4b6aef1c9a92e600449e7a1e057b745d7"))
-    (package
-      (name "emacs-hotfuzz")
-      (version (git-version "0" "1" commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/axelf4/hotfuzz")
-                      (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256 (base32 "0q96w0n30pfmnvr5ri0088m2fhb63qjcy6vahcn3ymjn4yjwkm8d"))))
-      (build-system emacs-build-system)
-      (arguments
-       `(#:modules ((guix build emacs-build-system)
-                    ((guix build cmake-build-system) #:prefix cmake:)
-                    (guix build emacs-utils)
-                    (guix build utils))
-         #:imported-modules (,@%emacs-build-system-modules
-                             (guix build cmake-build-system))
-         #:phases
-         (modify-phases %standard-phases
-           (add-after 'unpack 'patch-module-load
-             (lambda* (#:key outputs #:allow-other-keys)
-               (make-file-writable "hotfuzz.el")
-               (emacs-substitute-sexps "hotfuzz.el"
-                 ("(declare-function hotfuzz--filter-c"
-                  (string-append (assoc-ref outputs "out") "/lib/hotfuzz-module.so"))
-                 ("(use-module-p"
-                  `(module-load ,(string-append (assoc-ref outputs "out") "/lib/hotfuzz-module.so"))))))
-           (add-after 'build 'configure
-             (lambda* (#:key outputs #:allow-other-keys)
-               ((assoc-ref cmake:%standard-phases 'configure)
-                #:outputs outputs
-                #:out-of-source? #f
-                #:build-type "Release")))
-           (add-after 'configure 'make
-             (lambda* (#:key (make-flags '()) outputs #:allow-other-keys)
-               (apply invoke "cmake" "--build" "." make-flags)
-               (install-file
-                "hotfuzz-module.so"
-                (string-append (assoc-ref outputs "out") "/lib")))))
-         #:tests? #f))
-      (native-inputs (list cmake-minimal))
-      (home-page "https://github.com/axelf4/hotfuzz")
-      (synopsis "Fuzzy Emacs completion style")
-      (description "This is a fuzzy Emacs completion style similar to the built-in flex style, but with a better scoring algorithm.")
-      (license license:gpl3+))))
-
-(define emacs-magit-git
-  (package
-    (inherit emacs-magit)
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                     (url "https://github.com/magit/magit")
-                     (commit "3d1a008f1894dd087aee099629e3726877956033")))
-              (sha256 (base32 "11aiydzrfjbx7p72kapdsjbqkb72dj8zah666mx520msd7ywmsxj"))))))
+        (else (emacs-next))))
 
 (define without-tests
   ;; Disable the tests that fail on native-comp / emacs-next
   (options->transformation
    '((without-tests . "emacs-eldev")
-     (without-tests . "emacs-libgit"))))
+     (without-tests . "emacs-libgit")
+     (without-tests . "emacs-dash"))))
 
 (define elisp-packages
   (map without-tests
@@ -352,6 +107,7 @@ Server Protocol (LSP) client.")
         emacs-expand-region
         emacs-paredit
         emacs-devdocs
+        emacs-minuet
         ;; Langs
         emacs-cider
         emacs-sly
@@ -378,7 +134,8 @@ Server Protocol (LSP) client.")
         emacs-elpher
         emacs-pdf-tools
         emacs-libgit
-        emacs-magit-git
+        emacs-magit
+        emacs-pinentry
         emacs-vterm
         emacs-pcmpl-args
         emacs-eshell-syntax-highlighting
@@ -388,11 +145,7 @@ Server Protocol (LSP) client.")
         emacs-mwim
 
         ;; init-frames.el
-        (package
-          (inherit emacs-frames-only-mode)
-          (native-inputs
-           (modify-inputs (package-native-inputs emacs-frames-only-mode)
-             (replace "emacs-magit" emacs-magit-git)))))))
+        emacs-frames-only-mode)))
 
 (define-public packages
   (list
